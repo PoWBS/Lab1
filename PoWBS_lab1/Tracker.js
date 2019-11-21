3577c87b55adb641ead667bc8079430ee7278567
patrol95 <p.chojnacki1995@gmail.com>
2019-11-21 12:44:18 +0100

    got the tracker working reasonably okay-ish

--- PoWBS_lab1/Tracker.js
+++ PoWBS_lab1/Tracker.js
@@ -1,3 +1,22 @@
+(function ($) {
+    $.fn.inputFilter = function (inputFilter) {
+        return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function () {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            }
+        });
+    };
+
+
+
+}(jQuery));
+
+
 $(document).ready(function () {
 
 
@@ -17,29 +36,67 @@ $(document).ready(function () {
     }).trigger('change');
 
 
-    var $button = $('#addGameButton'),
-        $table = $('#matchTable > tbody');
+    var $buttonAdd = $('#addGameButton'),
+        $table = $('#matchTable'),
+        $tableBody = $('#matchTable > tbody');
 
-    $button.on('click', function () {
-        $table.prepend
-            ('<tr>' +
-                '<td>' + $("#modeselect option:selected").text() + '</td>' +
-                '<td>' + $("#mapselect option:selected").text() + '</td>' +
-                '<td>' + $("#roleselect option:selected").text() + '</td>' +
-                '<td>' + $("#heroselect option:selected").text() + '</td>' +
-                '<td>' + $("#result option:selected").text() + '</td>' +
-                '<td>' + $("#SRating").val() + '</td>' +
-                '<td>' + $("#comment").val() + '</td>' +
-                //todo add a delete button
-                '</tr>')
+
+
+
+
+
+
+
+
+    $buttonAdd.on('click', function () {
+        $tableBody.append
+            ('<tr class="customRow">' +
+                '<td class="modeCol">' + $('#modeselect option:selected').text() + '</td>' +
+                '<td class="mapCol">' + $('#mapselect option:selected').text() + '</td>' +
+                '<td class="roleCol">' + $('#roleselect option:selected').text() + '</td>' +
+                '<td class="heroCol">' + $('#heroselect option:selected').text() + '</td>' +
+                '<td class="resultCol">' + $('#result option:selected').text() + '</td>' +
+                '<td class="SRCol">' + $('#SRating').val() + '</td>' +
+                '<td class="commentCol">' + $('#comment').val() + '</td>' +
+                '<td>' +
+                '<button type="button" class="removebtn btn btn-secondary">Delete</button>' +
+                '</td>' +
+                '</tr>');
+
+    })
+
+    $table.on('click', '.removebtn', function () {
+        $(this).closest('tr').remove();
     })
 
 
-    /*todo
-    calculate average sr
-    input initial sr
-    calculate win ratio
-    show current sr
-    maybe input total sr instead and show difference, but complicated
-    */
+
+    $('#SRating').inputFilter(function (value) {
+        return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 5000);
+    });
+
+
+
+    $('#calculateStuff').on('click', function () {
+        $('#currentSR').text('Current SR: ' + $('td.SRCol:last').text());
+        $('#SRdifference').text('SR gain: ' +
+            srDiff($('td.SRCol:first'), $('td.SRCol:last')));
+        $('#ratio').text('Win ratio: ' + winRatio());
+    })
+
+    function winRatio() {
+        var wins = $('.resultCol:contains("Victory")').length,
+            games = $('.resultCol:contains("Defeat")').length + wins,
+            ratio = parseFloat(wins / games).toFixed(2);
+        return ratio;
+    }
+
+    function srDiff($first, $last) {
+        return parseInt($last.text() - $first.text());
+    }
+
+
+
 });
+
+
